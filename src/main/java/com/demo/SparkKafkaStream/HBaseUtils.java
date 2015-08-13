801b2a63ac77aae05c5ed06f6c96abdc9d366e05
Mohammed Ameen <mohammedameen23@gmail.com>
2015-08-13 19:47:10 +0530

    refine hbaseutils implementation

--- src/main/java/com/demo/SparkKafkaStream/HBaseUtils.java
+++ src/main/java/com/demo/SparkKafkaStream/HBaseUtils.java
@@ -4,9 +4,11 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.client.Get;
-import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.HConnection;
+import org.apache.hadoop.hbase.client.HConnectionManager;
+import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import org.apache.log4j.LogManager;
@@ -14,38 +16,43 @@ import org.apache.log4j.Logger;
 
 public class HBaseUtils {
 
-	private static Logger logger = LogManager.getLogger(HBaseUtils.class.getName());
+    private static Logger logger = LogManager.getLogger(HBaseUtils.class.getName());
 
     private Configuration hConfig;
+    private HConnection connection;
+    private HTableInterface theTable;
 
     public HBaseUtils() {
-        hConfig = HBaseConfiguration.create();
-        hConfig.addResource(new Path("file:///opt/mapr/hbase/hbase-0.98.12/conf/hbase-site.xml"));
+        try {
+            //hConfig.addResource(new Path("file:///opt/mapr/hbase/hbase-0.98.12/conf/hbase-site.xml"));
+            hConfig = HBaseConfiguration.create();
+            connection = HConnectionManager.createConnection(hConfig);
+        } catch (Exception ex) {
+            logger.info("Exception while init: " + ex.toString());
+        }
     }
 
     public void table_put(String tableName, String rowKey, String colFamily,
                           String col, String val) throws Exception {
 
-		logger.info("Put on: " + tableName + " -> " + rowKey + " " + colFamily + ":" + col + " = " + val);
-        HTable table = new HTable(hConfig, tableName);
+        logger.info("Put on: " + tableName + " -> " + rowKey + " " + colFamily + ":" + col + " = " + val);
+        theTable = connection.getTable(tableName);
         Put p = new Put(Bytes.toBytes(rowKey));
-        p.add(Bytes.toBytes(colFamily), Bytes.toBytes(col),
-                Bytes.toBytes(val));
-        table.put(p);
-        table.close();
+        p.add(Bytes.toBytes(colFamily), Bytes.toBytes(col),Bytes.toBytes(val));
+        theTable.put(p);
+        theTable.close();
     }
 
     public String table_get(String tableName, String rowKey, String colFamily,
             String col) throws Exception {
 
-		logger.info("Get on: " + tableName + " -> " + rowKey + " " + colFamily + ":" + col);
-        HTable table = new HTable(hConfig, tableName);
+        logger.info("Get on: " + tableName + " -> " + rowKey + " " + colFamily + ":" + col);
+        theTable = connection.getTable(tableName);
         Get g = new Get(Bytes.toBytes(rowKey));
-        Result r = table.get(g);
-        byte[] value = r.getValue(Bytes.toBytes(colFamily), Bytes
-                .toBytes(col));
+        Result r = theTable.get(g);
+        byte[] value = r.getValue(Bytes.toBytes(colFamily), Bytes.toBytes(col));
         String strVal = Bytes.toString(value);
-        table.close();
+        theTable.close();
         return strVal;
     }
 }
